f0f74197d5772e01e1eb23b06e8c27e899043c14
shoghdev <shogher.fordev@gmail.com>
2026-06-06 01:01:20 +0400

    Fix issue with emailJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ConfigProvider, theme as antdTheme } from 'antd';
+import { App as AntApp, ConfigProvider, theme as antdTheme } from 'antd';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SiteLayout } from '@/components/common/SiteLayout';
@@ -29,16 +29,18 @@ const App = () => {
           algorithm: mode === 'dark' ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm,
         }}
       >
-        <BrowserRouter>
-          <SiteLayout themeMode={mode} onThemeToggle={toggleMode}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/experience" element={<ExperiencePage />} />
-              <Route path="/contact" element={<ContactPage />} />
-            </Routes>
-          </SiteLayout>
-        </BrowserRouter>
+        <AntApp>
+          <BrowserRouter>
+            <SiteLayout themeMode={mode} onThemeToggle={toggleMode}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/experience" element={<ExperiencePage />} />
+                <Route path="/contact" element={<ContactPage />} />
+              </Routes>
+            </SiteLayout>
+          </BrowserRouter>
+        </AntApp>
       </ConfigProvider>
     </QueryClientProvider>
   );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_EMAILJS_SERVICE_ID: string;
   readonly VITE_EMAILJS_TEMPLATE_ID: string;
   readonly VITE_EMAILJS_PUBLIC_KEY: string;
+  readonly VITE_EMAILJS_TO_EMAIL: string;
 }
 
 interface ImportMeta {
